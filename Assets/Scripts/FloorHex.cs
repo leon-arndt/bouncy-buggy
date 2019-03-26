ce48b96dc375bc8fa6d4f1f49c3d73cca7b5a3f2
Leon Arndt <neotalon27@gmail.com>
2019-03-26 14:49:37 +0100

    Added hex tiles to replace the death plane

---
+++ Assets/Scripts/FloorHex.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorHex : MonoBehaviour
+{
+    private Renderer rend;
+    private float randomOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<MeshRenderer>() != null)
+        {
+            rend = GetComponent<MeshRenderer>();
+            rend.material.color = Color.white;
+        }
+
+        randomOffset = Random.Range(0, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rend != null)
+        {
+            rend.material.color = Color.Lerp(Color.gray, Color.black, Mathf.PingPong(Time.time + randomOffset, 1));
+            transform.Translate(Mathf.Sin(randomOffset * 0.1f * Time.time) * 0.05f *  Vector3.up, Space.World);
+        }
+    }
+}
